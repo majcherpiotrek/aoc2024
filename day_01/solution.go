8a817e15bdd95939793d143f02b5b3bb3be894e8
Piotr Majcher <majcherpiotr.dev@gmail.com>
2024-12-02 16:06:54 +0800

    day 2 scaffolding and small refactor

--- day_01/solution.go
+++ day_01/solution.go
@@ -1,45 +1,12 @@
 package day_01
 
 import (
-	"fmt"
+	"aoc2024/common"
 	"slices"
-	"strconv"
-	"strings"
 )
 
-func parseInput(input *[]string) ([][]int, error) {
-	var column_1 []int
-	var column_2 []int
-
-	for i, line := range *input {
-		words := strings.Fields(line)
-
-		if len(words) != 2 {
-			return [][]int{}, fmt.Errorf("Invalid input at line %d - expected exactly two columns", i)
-		}
-
-		value_1, err := strconv.Atoi(words[0])
-
-		if err != nil {
-			return [][]int{}, fmt.Errorf("Invalid value at line %d, column 0: %s - Integer expected", i, words[0])
-		}
-
-		value_2, err := strconv.Atoi(words[1])
-
-		if err != nil {
-			return [][]int{}, fmt.Errorf("Invalid value at line %d, column 1: %s - Integer expected", i, words[1])
-		}
-
-		column_1 = append(column_1, value_1)
-		column_2 = append(column_2, value_2)
-	}
-
-	return append([][]int{}, column_1, column_2), nil
-
-}
-
 func Part1(input *[]string) (int, error) {
-	data, err := parseInput(input)
+	data, err := common.ParseInput(input)
 
 	if err != nil {
 		return -1, err
@@ -84,7 +51,7 @@ func calculateOccurrences(secondList *[]int) map[int]int {
 }
 
 func Part2(input *[]string) (int, error) {
-	data, err := parseInput(input)
+	data, err := common.ParseInput(input)
 
 	if err != nil {
 		return -1, err
---
+++ day_02/solution.go
@@ -0,0 +1,15 @@
+package day_02
+
+import (
+	"fmt"
+)
+
+func Part1(input *[]string) (int, error) {
+
+	return -1, fmt.Errorf("not implemented")
+}
+
+func Part2(input *[]string) (int, error) {
+
+	return -1, fmt.Errorf("not implemented")
+}
