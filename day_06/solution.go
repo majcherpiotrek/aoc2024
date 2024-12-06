22ddfa313dc2ae80450a521c882f37931e371e90
Piotr Majcher <majcherpiotr.dev@gmail.com>
2024-12-06 18:17:03 +0800

    small refactor

--- day_06/solution.go
+++ day_06/solution.go
@@ -2,6 +2,7 @@ package day_06
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -32,13 +33,10 @@ func applyMove(pos []int, move []int) []int {
 	return []int{pos[0] + move[0], pos[1] + move[1]}
 }
 
-func Part1(rows *[]string) (int, error) {
+func getGuardStartPosition(rows *[]string) ([]int, error) {
 	guardPosition := make([]int, 0, 2)
-	width := len((*rows)[0])
-	height := len(*rows)
 
 	for y, row := range *rows {
-
 		guardX := strings.IndexByte(row, '^')
 
 		if guardX != -1 {
@@ -48,8 +46,16 @@ func Part1(rows *[]string) (int, error) {
 	}
 
 	if len(guardPosition) == 0 {
-		return -1, fmt.Errorf("Could not find the guard")
+		return []int{}, fmt.Errorf("Could not find the guard")
 	}
+
+	return guardPosition, nil
+}
+
+func getVisitedPositions(rows *[]string, guardStartPosition []int) map[string]struct{} {
+	guardPosition := []int{guardStartPosition[0], guardStartPosition[1]}
+	width := len((*rows)[0])
+	height := len(*rows)
 
 	visitedPositions := make(map[string]struct{})
 	direction := Up
@@ -75,9 +81,84 @@ func Part1(rows *[]string) (int, error) {
 		}
 	}
 
+	return visitedPositions
+}
+
+func Part1(rows *[]string) (int, error) {
+	guardPosition, err := getGuardStartPosition(rows)
+
+	if err != nil {
+		return -1, err
+	}
+
+	visitedPositions := getVisitedPositions(rows, guardPosition)
+
 	return len(visitedPositions), nil
 }
 
 func Part2(rows *[]string) (int, error) {
-	return -1, fmt.Errorf("not implemented")
+	guardStartPosition, err := getGuardStartPosition(rows)
+	width := len((*rows)[0])
+	height := len(*rows)
+
+	if err != nil {
+		return -1, err
+	}
+
+	visitedPositions := getVisitedPositions(rows, guardStartPosition)
+
+	var validBlockades [][]int
+
+	for pos := range visitedPositions {
+		posStrings := strings.Split(pos, "-")
+
+		x, err := strconv.Atoi(posStrings[0])
+		if err != nil {
+			return -1, err
+		}
+
+		y, err := strconv.Atoi(posStrings[1])
+		if err != nil {
+			return -1, err
+		}
+
+		if x == guardStartPosition[0] && y == guardStartPosition[1] {
+			continue
+		}
+
+		blockade := []int{x, y}
+		loopCheck := make(map[string]struct{})
+		guardPosition := []int{guardStartPosition[0], guardStartPosition[1]}
+		direction := Up
+
+		for guardPosition[0] >= 0 && guardPosition[0] < width && guardPosition[1] >= 0 && guardPosition[1] < height {
+			positionKey := fmt.Sprintf("%d-%d-%d", guardPosition[0], guardPosition[1], direction)
+			_, hasLooped := loopCheck[positionKey]
+
+			if hasLooped {
+				validBlockades = append(validBlockades, blockade)
+				break
+			}
+
+			loopCheck[positionKey] = struct{}{}
+
+			var nextPosition []int
+
+			move := getMoveForDirection(direction)
+			nextPosition = applyMove(guardPosition, move)
+
+			if nextPosition[0] < 0 || nextPosition[0] >= width || nextPosition[1] < 0 || nextPosition[1] >= height {
+				break
+			}
+
+			if (*rows)[nextPosition[1]][nextPosition[0]] == '#' || (nextPosition[0] == blockade[0] && nextPosition[1] == blockade[1]) {
+				// Turn right
+				direction = (direction + 1) % 4
+			} else {
+				guardPosition = nextPosition
+			}
+		}
+	}
+
+	return len(validBlockades), nil
 }
