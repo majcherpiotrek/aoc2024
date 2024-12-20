567e8c4479ac2238e09a302a0dd6d93ea36c6280
Piotr Majcher <majcherpiotr.dev@gmail.com>
2024-12-20 18:25:36 +0100

    day 19 part 2 working

--- day_19/solution.go
+++ day_19/solution.go
@@ -74,96 +74,40 @@ func getPadding(depth int) string {
 	return padding
 }
 
-func findArrangements(design string, availablePatterns *[]string, canBuildDesignCache *map[string]bool, arrCache *map[string]map[string]struct{}, depth int) map[string]struct{} {
-	firstPrintPadding := getFirstPrintPadding(depth)
-	padding := getPadding(depth)
-	// fmt.Printf("%sCalled with design %s\n", firstPrintPadding, design)
-	// fmt.Printf("CACHE %v\n\n", *cache)
-	fmt.Printf("%s%dStarting design %s\n", firstPrintPadding, depth, design)
-	canBuild, alreadyChecked := (*canBuildDesignCache)[design]
-	if !alreadyChecked {
-		canBuild = canBuildDesign(design, availablePatterns, canBuildDesignCache)
+func findArrangements(design string, availablePatterns *[]string, possibleDesigns *map[string]bool, cache *map[string]int) int {
+	isPossible, isInPossibleCache := (*possibleDesigns)[design]
+	if isInPossibleCache && !isPossible {
+		return 0
 	}
 
-	if !canBuild {
-		fmt.Printf("%s%d Can't build this design\n", padding, depth)
-		return map[string]struct{}{}
-	}
-
-	cachedArrangements, isInCache := (*arrCache)[design]
+	cacheValue, isInCache := (*cache)[design]
 	if isInCache {
-		fmt.Printf("%s%dCACHE HIT design %s\n", padding, depth, design)
-		return cachedArrangements
+		return cacheValue
 	}
-	fmt.Printf("%s%d No cache hit design %s\n", padding, depth, design)
 
-	arrangements := make(map[string]struct{})
+	arrangements := 0
 
 	for _, pattern := range *availablePatterns {
-		fmt.Printf("%s%dChecking pattern %s for design %s\n", padding, depth, pattern, design)
+		patternLen := len(pattern)
+		if len(design) < patternLen {
+			continue
+		}
+
 		if design == pattern {
-			// fmt.Printf("%sPattern equals design\n", padding)
-			arrangements[pattern] = struct{}{}
-			(*arrCache)[pattern] = map[string]struct{}{
-				pattern: {},
-			}
-
-			if len(design) == 1 {
-				return arrangements
-			}
+			arrangements++
 			continue
 		}
 
-		indexOfPattern := strings.Index(design, pattern)
-
-		if indexOfPattern == -1 {
-			// fmt.Printf("%sPattern not found\n", padding)
-			continue
-		}
-		// fmt.Printf("%sPattern %s present at %d. Going deeper ->\n", padding, pattern, indexOfPattern)
-
-		leadingSlice := design[0:indexOfPattern]
-		//fmt.Printf("%sLeading slice: %v\n", padding, leadingSlice)
-		arrangementsLeading := make(map[string]struct{})
-		if len(leadingSlice) > 0 {
-			arrangementsLeading = findArrangements(leadingSlice, availablePatterns, canBuildDesignCache, arrCache, depth+1)
-		}
-
-		trailingSlice := design[indexOfPattern+len(pattern):]
-		//fmt.Printf("%sTrailing slice: %v\n", padding, leadingSlice)
-		arrangementsTrailing := make(map[string]struct{})
-		if len(trailingSlice) > 0 {
-			arrangementsTrailing = findArrangements(trailingSlice, availablePatterns, canBuildDesignCache, arrCache, depth+1)
-		}
-
-		if len(leadingSlice) == 0 {
-			fmt.Printf("%s%d Leading slice empty", padding, depth)
-			for arr := range arrangementsTrailing {
-				arrangements[fmt.Sprintf("%s,%s", pattern, arr)] = struct{}{}
-			}
-			continue
-		}
-
-		if len(trailingSlice) == 0 {
-			fmt.Printf("%s%d Trailing slice empty", padding, depth)
-			for arr := range arrangementsLeading {
-				arrangements[fmt.Sprintf("%s,%s", arr, pattern)] = struct{}{}
-			}
-			continue
-		}
-
-		if len(arrangementsLeading) > 0 && len(arrangementsTrailing) > 0 {
-			fmt.Printf("%s%d Merging possibilities from leading (%d) and trailing (%d)\n", padding, depth, len(arrangementsLeading), len(arrangementsTrailing))
-			for leading := range arrangementsLeading {
-				for trailing := range arrangementsTrailing {
-					arr := fmt.Sprintf("%s,%s,%s", leading, pattern, trailing)
-					arrangements[arr] = struct{}{}
-				}
+		if design[0:patternLen] == pattern {
+			designSlice := design[patternLen:]
+			tailArrangements := findArrangements(designSlice, availablePatterns, possibleDesigns, cache)
+			if tailArrangements > 0 {
+				arrangements += tailArrangements
 			}
 		}
 	}
 
-	(*arrCache)[design] = arrangements
+	(*cache)[design] = arrangements
 	return arrangements
 }
 
@@ -204,15 +148,20 @@ func Part2(input *[]string) (int, error) {
 		return len(b) - len(a)
 	})
 
-	arrangementsCache := make(map[string]map[string]struct{})
-	canBuildDesignCache := make(map[string]bool)
+	possibleDesignsCache := make(map[string]bool)
+	for i := 2; i < len(*input); i++ {
+		design := (*input)[i]
+		canBuildDesign(design, &availablePatterns, &possibleDesignsCache)
+	}
+
+	arrangementsCache := make(map[string]int)
+
 	sum := 0
 	for i := 2; i < len(*input); i++ {
-		fmt.Printf("Design %d\n", i-1)
 		design := (*input)[i]
-		arrangements := findArrangements(design, &availablePatterns, &canBuildDesignCache, &arrangementsCache, 0)
-		fmt.Printf("%s: %d\n", design, len(arrangements))
-		sum += len(arrangements)
+		arrangements := findArrangements(design, &availablePatterns, &possibleDesignsCache, &arrangementsCache)
+		fmt.Printf("%s: %d\n", design, arrangements)
+		sum += arrangements
 	}
 
 	return sum, nil
